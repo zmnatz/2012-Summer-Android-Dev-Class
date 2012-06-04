ed31f09264e0b906a9363fe62b1ad81b7ecd83e7
Zaven Mnatzakanian <zmnatz@gmail.com>
2012-06-04 18:19:03 -0400

    Finished Homework 1, Hello Android

---
+++ hw1/src/mnatzakanian/zaven/hw1/Hw1Activity.java
@@ -0,0 +1,13 @@
+package mnatzakanian.zaven.hw1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hw1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
