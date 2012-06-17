4b6143235be8b2c25eabb17553869f5207763996
Zaven Mnatzakanian <zmnatz@gmail.com>
2012-06-17 10:58:39 -0400

    updated resources

--- hw2/gen/mnatzakanian/zaven/hw2/R.java
+++ hw2/gen/mnatzakanian/zaven/hw2/R.java
@@ -14,16 +14,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int contactList=0x7f050009;
-        public static final int displayName=0x7f050003;
-        public static final int emailAddr=0x7f050007;
-        public static final int firstName=0x7f050002;
-        public static final int homeNumber=0x7f050004;
-        public static final int lastName=0x7f050000;
-        public static final int mobileNumber=0x7f050006;
-        public static final int nameSeparator=0x7f050001;
-        public static final int phoneNumber=0x7f050008;
-        public static final int workNumber=0x7f050005;
+        public static final int birthday=0x7f060004;
+        public static final int cancelButton=0x7f06000a;
+        public static final int contactList=0x7f06000c;
+        public static final int createItem=0x7f06000d;
+        public static final int displayName=0x7f060003;
+        public static final int editItem=0x7f06000e;
+        public static final int emailAddr=0x7f060008;
+        public static final int firstName=0x7f060002;
+        public static final int homeNumber=0x7f060005;
+        public static final int lastName=0x7f060000;
+        public static final int mobileNumber=0x7f060007;
+        public static final int nameSeparator=0x7f060001;
+        public static final int phoneNumber=0x7f06000b;
+        public static final int submitButton=0x7f060009;
+        public static final int workNumber=0x7f060006;
     }
     public static final class layout {
         public static final int contact_display=0x7f030000;
@@ -31,17 +36,29 @@ public final class R {
         public static final int contact_entry=0x7f030002;
         public static final int main=0x7f030003;
     }
+    public static final class menu {
+        public static final int contact_list_menu=0x7f050000;
+        public static final int contact_menu=0x7f050001;
+    }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int birthday_label=0x7f040004;
-        public static final int comma=0x7f04000a;
+        public static final int born_label=0x7f040005;
+        public static final int cancel=0x7f04000d;
+        public static final int comma=0x7f04000b;
+        public static final int create_contact_label=0x7f040010;
         public static final int display_name_label=0x7f040003;
-        public static final int email_label=0x7f040009;
+        public static final int edit_contact_label=0x7f04000f;
+        public static final int email_label=0x7f04000a;
+        public static final int empty_list_label=0x7f04000e;
         public static final int first_name_label=0x7f040001;
-        public static final int home_phone_label=0x7f040006;
+        public static final int home_phone_label=0x7f040007;
         public static final int last_name_label=0x7f040002;
-        public static final int mobile_phone_label=0x7f040008;
-        public static final int phone_label=0x7f040005;
-        public static final int work_phone_label=0x7f040007;
+        public static final int mobile_phone_label=0x7f040009;
+        public static final int personal_info=0x7f040012;
+        public static final int phone_label=0x7f040006;
+        public static final int save=0x7f04000c;
+        public static final int set_birthday=0x7f040011;
+        public static final int work_phone_label=0x7f040008;
     }
 }
