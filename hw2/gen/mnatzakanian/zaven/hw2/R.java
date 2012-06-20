747478c2d23ab733dc05933dd22dff9ce7fc0bf3
Zaven Mnatzakanian <zmnatz@gmail.com>
2012-06-20 16:53:06 -0400

    New Project

--- hw2/gen/mnatzakanian/zaven/hw2/R.java
+++ hw2/gen/mnatzakanian/zaven/hw2/R.java
@@ -20,15 +20,15 @@ public final class R {
         public static final int createItem=0x7f06000d;
         public static final int displayName=0x7f060003;
         public static final int editItem=0x7f06000e;
-        public static final int emailAddr=0x7f060008;
+        public static final int emailAddr=0x7f060005;
         public static final int firstName=0x7f060002;
-        public static final int homeNumber=0x7f060005;
+        public static final int homeNumber=0x7f060006;
         public static final int lastName=0x7f060000;
-        public static final int mobileNumber=0x7f060007;
+        public static final int mobileNumber=0x7f060008;
         public static final int nameSeparator=0x7f060001;
         public static final int phoneNumber=0x7f06000b;
         public static final int submitButton=0x7f060009;
-        public static final int workNumber=0x7f060006;
+        public static final int workNumber=0x7f060007;
     }
     public static final class layout {
         public static final int contact_display=0x7f030000;
---
+++ hw3/gen/mnatzakanian/zaven/hw3/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package mnatzakanian.zaven.hw3;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
